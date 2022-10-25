d23b1c87b75677d3981027c42db44c3c110eed1a
Ramazan <laumanov.r@gmail.com>
2022-10-25 18:10:07 +0600

    modify main js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import VueToast from 'vue-toast-notification';
+import './assets/styles/main.scss';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueToast, {position: 'top-right', duration: 1500});
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	vuetify,
+	render: h => h(App)
+}).$mount('#app');
